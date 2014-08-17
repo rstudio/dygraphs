c5cfc7bed379b586eb569b24e889b2632dbd39a7
JJ Allaire <jj@rstudio.org>
2014-08-17 06:50:18 -0400

    tweak example spacing and order

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -2,12 +2,15 @@
 library(dygraphs)
 
 dygraph(discoveries, title = "Important Discoveries",
-  yaxis = dyAxis(label = "Total / Year"),
+        
   xaxis = dyAxis(label = "Discoveries", legend = "Discoveries"),
+  yaxis = dyAxis(label = "Total / Year"),
+  
   interaction = dyInteraction(legend = "onmouseover",
                               showRangeSelector = TRUE,
                               showLabelsOnHighlight = TRUE,
                               showRoller = FALSE),
+  
   theme = dyTheme(titleHeight = 24,
                   xLabelHeight = 18,
                   yLabelWidth = 18)
