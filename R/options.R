eefb647027f63fdb18219ce1545dbd2df7ced87e
JJ Allaire <jj@rstudio.org>
2014-09-05 12:02:07 -0400

    add stackedGraph option

--- R/options.R
+++ R/options.R
@@ -3,12 +3,15 @@
 #' 
 #' Add options to a dygraph plot.
 #' 
+#' @param stackedGraph If set, stack series on top of one another rather than
+#'   drawing them independently. The first series specified in the input data
+#'   will wind up on top of the chart and the last will be on bottom.
 #' @param fillGraph Should the area underneath the graph be filled? This option 
 #'   is not compatible with error bars. This option can also be set on a 
 #'   per-series basis via \code{\link{dySeries}}.
 #' @param drawPoints Draw a small dot at each point, in addition to a line going
 #'   through the point. This makes the individual data points easier to see, but
-#'   can increase visual clutter in the chart. This option can also be set on a
+#'   can increase visual clutter in the chart. This option can also be set on a 
 #'   per-series basis via \code{\link{dySeries}}.
 #' @param pointSize The size of the dot to draw on each point in pixels. A dot 
 #'   is always drawn when a point is "isolated", i.e. there is a missing point 
@@ -21,11 +24,13 @@
 #' @return Series options
 #'   
 #' @export
-dyOptions <- function(fillGraph = FALSE,
+dyOptions <- function(stackedGraph = FALSE,
+                      fillGraph = FALSE,
                       drawPoints = FALSE,
                       pointSize = 1,
                       ...) {
   options <- list()
+  options$stackedGraph <- stackedGraph
   options$fillGraph <- fillGraph
   options$drawPoints <- drawPoints
   options$pointSize <- pointSize
