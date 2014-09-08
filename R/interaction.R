8fe86621b1864e4b85722f40458fdff534fd4de2
JJ Allaire <jj@rstudio.org>
2014-09-08 17:24:29 -0400

    break out dyRangeSelector function

--- R/interaction.R
+++ R/interaction.R
@@ -4,12 +4,6 @@
 #' Add interactive behaviors to a dygraph plot.
 #' 
 #' @param dygraph Dygraph to add interactive behaviors to
-#' @param showRangeSelector Show or hide the range selector widget.
-#' @param dateWindow Initially zoom in on a section of the graph. Is a two 
-#'   element vector [earliest, latest], where earliest/latest objects 
-#'   convertable via \code{as.POSIXct}.
-#' @param animatedZooms Set this option to animate the transition between zoom 
-#'   windows.
 #' @param highlightCircleSize The size in pixels of the dot drawn over 
 #'   highlighted points.
 #' @param highlightSeriesBackgroundAlpha Fade the background while highlighting 
@@ -29,9 +23,6 @@
 #'   
 #' @export
 dyInteraction <- function(dygraph,
-                          showRangeSelector = FALSE,
-                          dateWindow = NULL,
-                          animatedZooms = FALSE,
                           highlightCircleSize = 3,
                           highlightSeriesBackgroundAlpha = 0.5,
                           highlightSeriesOpts = list(),
@@ -40,17 +31,6 @@ dyInteraction <- function(dygraph,
                           showRoller = FALSE,
                           rollPeriod = 1) {
   interaction <- list()
-  interaction$showRangeSelector <- showRangeSelector
-  if (!is.null(dateWindow)) {
-    if (length(dateWindow) != 2)
-      stop("dateWindow must be vector of length 2 that is convertible to POSIXct")
-    interaction$dateWindow <- sapply(USE.NAMES = FALSE, dateWindow, 
-       function(x) { 
-         as.double(as.POSIXct(x, tz = "GMT")) * 1000; 
-       }
-    );
-  }
-  interaction$animatedZooms <- animatedZooms
   interaction$highlightCircleSize <- highlightCircleSize
   interaction$highlightSeriesBackgroundAlpha <- highlightSeriesBackgroundAlpha
   interaction$highlightSeriesOpts <- highlightSeriesOpts
@@ -65,3 +45,55 @@ dyInteraction <- function(dygraph,
   # return modified dygraph
   dygraph
 }
+
+
+#' Interactive selection and zooming of date ranges
+#' 
+#' Add a range selector to the bottom of the chart that allows users to pan and 
+#' zoom to various date ranges.
+#' 
+#' @param dygraph Dygraph to add range selector to
+#' @param dateWindow Initially zoom in on a section of the graph. Is a two 
+#'   element vector [earliest, latest], where earliest/latest objects 
+#'   convertable via \code{as.POSIXct}.
+#' @param height Height, in pixels, of the range selector widget. This option 
+#'   can only be specified at Dygraph creation time.
+#' @param fillColor The range selector mini plot fill color. This can be of the 
+#'   form "#AABBCC" or "rgb(255,100,200)" or "yellow". You can also specify 
+#'   \code{NULL} or "" to turn off fill.
+#' @param strokeColor The range selector mini plot stroke color. This can be of
+#'   the form "#AABBCC" or "rgb(255,100,200)" or "yellow". You can also specify
+#'   \code{NULL} or "" to turn off stroke.
+#'   
+#' @return A dygraph that displays a range selector
+#'   
+#' @export
+dyRangeSelector <- function(dygraph,
+                            dateWindow = NULL, 
+                            height = 40,
+                            fillColor = " #A7B1C4",
+                            strokeColor = "#808FAB") {
+  
+  selector <- list()
+  selector$showRangeSelector = TRUE
+  if (!is.null(dateWindow)) {
+    if (length(dateWindow) != 2)
+      stop("dateWindow must be vector of length 2 that is convertible to POSIXct")
+    selector$dateWindow <- sapply(USE.NAMES = FALSE, dateWindow, 
+                                     function(x) { 
+                                       as.double(as.POSIXct(x, tz = "GMT")) * 1000; 
+                                     }
+    );
+  }
+  selector$height <- height
+  selector$rangeSelectorPlotFillColor <- fillColor
+  selector$rangeSelectorPlotStrokeColor <- strokeColor
+  
+  # merge selector
+  dygraph$x$attrs <- mergeLists(dygraph$x$attrs, selector)
+  
+  # return modified dygraph
+  dygraph
+  
+}
+
