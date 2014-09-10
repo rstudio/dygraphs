9c0fcf0901e079d39cb6ce3b557909501aa71ffa
JJ Allaire <jj@rstudio.org>
2014-09-10 14:32:37 -0400

    add docs for shiny bindings

--- R/dygraph.R
+++ R/dygraph.R
@@ -72,13 +72,32 @@ dygraph <- function(data, main = NULL, xlab = NULL, ylab = NULL,
   )
 }
 
+
+#' Shiny bindings for dygraph
+#' 
+#' Output and render functions for using dygraph within Shiny 
+#' applications and interactive Rmd documents.
+#' 
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{"100\%"},
+#'   \code{"400px"}, \code{"auto"}) or a number, which will be coerced to a
+#'   string and have \code{"px"} appended.
+#' @param expr An expression that generates a networkD3 graph
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This 
+#'   is useful if you want to save an expression in a variable.
+#'   
+#' @name dygraph-shiny
+#'
 #' @export
 dygraphOutput <- function(outputId, width = "100%", height = "400px") {
   htmlwidgets::shinyWidgetOutput(outputId, "dygraphs", width, height)
 }
 
+#' @rdname dygraph-shiny
 #' @export
 renderDygraph <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
   htmlwidgets::shinyRenderWidget(expr, dygraphOutput, env, quoted = TRUE)
 }
+
