563946e110096299f9fa8a22f4339a71f147f39b
JJ Allaire <jj@rstudio.org>
2014-09-11 11:00:19 -0400

    add labelsSeparateLines option

--- R/legend.R
+++ R/legend.R
@@ -5,37 +5,40 @@
 #' @param dygraph Dygraph to configure legend options for.
 #' @param show When to display the legend. Specify "always" to always show the 
 #'   legend. Specify "onmouseover" to only display it when a user mouses over 
-#'   the chart. The default behavior is "auto", which results in "always" when
+#'   the chart. The default behavior is "auto", which results in "always" when 
 #'   more than one series is plotted and "onmouseover" when only a single series
 #'   is plotted.
 #' @param width Width (in pixels) of the div which shows the legend.
 #' @param showZeroValues Show zero value labels in the legend.
-#' @param externalDiv Show data labels in an external div, rather than on the 
+#' @param labelsDiv Show data labels in an external div, rather than on the 
 #'   graph. This value should be a div element id.
+#' @param labelsSeparateLines Put a <br/> between lines in the label string.
+#'   Often used in conjunction with \code{labelsDiv}.
 #' @param hideOnMouseOut Whether to hide the legend when the mouse leaves the 
 #'   chart area. This option applies when \code{show} is set to "onmouseover". 
-#'   Note that this also affects the hiding of the \code{\link{dyHighlight}} 
-#'   on mouse out.
+#'   Note that this also affects the hiding of the \code{\link{dyHighlight}} on
+#'   mouse out.
 #'   
 #' @return A dygraph with customized legend options
-#' 
-#' @note
-#' See the \href{http://jjallaire.github.io/dygraphs/}{online documentation} for
-#' additional details and examples.  
 #'   
+#' @note See the \href{http://jjallaire.github.io/dygraphs/}{online
+#' documentation} for additional details and examples.
+#' 
 #' @export
 dyLegend <- function(dygraph,
                      show = c("auto", "always", "onmouseover"),
                      width = 250,
                      showZeroValues = TRUE,
-                     externalDiv = NULL,
+                     labelsDiv = NULL,
+                     labelsSeparateLines = FALSE,
                      hideOnMouseOut = TRUE) {
   
   legend <- list()
   legend$legend <- match.arg(show)
   legend$labelsDivWidth <- width
   legend$labelsShowZeroValues <- showZeroValues
-  legend$labelsDiv <- externalDiv
+  legend$labelsDiv <- labelsDiv
+  legend$labelsSeparateLines <- labelsSeparateLines
   legend$hideOverlayOnMouseOut <- hideOnMouseOut
    
   # merge legend
