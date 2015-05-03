2ed833054e060386e7ea5fe784179ca7eb4bba12
jrowen <jonathanro@gmail.com>
2015-05-03 12:33:29 +0000

    Documentation clean-up

--- R/event.R
+++ R/event.R
@@ -5,13 +5,13 @@
 #' @param dygraph Dygraph to add event line to
 #' @param date Date/time for the event (must be a \code{POSIXct} object or 
 #'   another object convertible to \code{POSIXct} via \code{as.POSIXct})
-#' @param label Optional label for event.
+#' @param label Label for event. Defaults to blank.
 #' @param labelLoc Location for label (top or bottom)
 #' @param color Color of event line. This can be of the form "#AABBCC" or 
 #'   "rgb(255,100,200)" or "yellow". Defaults to black.
 #' @param strokePattern A predefined stroke pattern type ("dotted", "dashed",
 #'   "dotdash", or "solid") or a custom pattern array where the even index is 
-#'   a draw and odd is a space in pixels.
+#'   a draw and odd is a space in pixels. Defaults to dashed.
 #'   
 #' @return A dygraph with the specified event line.
 #'   
@@ -29,15 +29,15 @@
 #' @export
 dyEvent <- function(dygraph, 
                     date,
-                    label, 
+                    label = NULL, 
                     labelLoc = c("top", "bottom"),
                     color = "black", 
                     strokePattern = "dashed") {
   
   # create event
   event <- list()
-  event$pos = asISO8601Time(date)
-  if (!missing(label) && !is.null(label)) event$label <- label
+  event$pos <- asISO8601Time(date)
+  event$label <- label
   event$labelLoc <- match.arg(labelLoc)
   event$color <- color
   event$strokePattern <- resolveStrokePattern(strokePattern)
@@ -55,14 +55,14 @@ dyEvent <- function(dygraph,
 #' Add a horizontal limit line to a dygraph
 #' 
 #' @param dygraph Dygraph to add limit line to
-#' @param limit numeric position of limit.
-#' @param label Optional label for limit.
+#' @param limit Numeric position of limit.
+#' @param label Label for limit. Defaults to blank.
 #' @param labelLoc Location for label (left or right).
 #' @param color Color of limit line. This can be of the form "#AABBCC" or 
 #'   "rgb(255,100,200)" or "yellow". Defaults to black.
 #' @param strokePattern A predefined stroke pattern type ("dotted", "dashed",
 #'   "dotdash", or "solid") or a custom pattern array where the even index is 
-#'   a draw and odd is a space in pixels.
+#'   a draw and odd is a space in pixels. Defaults to dashed.
 #'   
 #' @return A dygraph with the specified limit line.
 #'   
@@ -74,21 +74,21 @@ dyEvent <- function(dygraph,
 #' 
 #' dygraph(presidents, main = "Presidential Approval") %>%
 #'   dyAxis("y", valueRange = c(0, 100)) %>% 
-#'   dyLimit(max(presidents, na.rm = TRUE), "Max", axis = "y",
+#'   dyLimit(max(presidents, na.rm = TRUE), "Max",
 #'           strokePattern = "solid", color = "blue")
 #'  
 #' @export
 dyLimit <- function(dygraph, 
                     limit,
-                    label, 
+                    label = NULL, 
                     labelLoc = c("left", "right"),
                     color = "black", 
                     strokePattern = "dashed") {
   
   # create event
   event <- list()
-  event$pos = limit
-  if (!missing(label) && !is.null(label)) event$label <- label
+  event$pos <- limit
+  event$label <- label
   event$labelLoc <- match.arg(labelLoc)
   event$color <- color
   event$strokePattern <- resolveStrokePattern(strokePattern)
