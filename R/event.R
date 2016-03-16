b4697779b5e1d11f051be2ad8c23a697a5f6bae2
Michel Kuhlmann <kum@tkconsult.ch>
2016-03-16 10:59:06 +0100

    update NEWS and add example

--- R/event.R
+++ R/event.R
@@ -28,6 +28,10 @@
 #'   dyAxis("y", valueRange = c(0, 100)) %>%
 #'   dyEvent("1950-6-30", "Korea", labelLoc = "bottom") %>%
 #'   dyEvent("1965-2-09", "Vietnam", labelLoc = "bottom")
+
+#' dygraph(presidents, main = "Presidential Approval") %>%
+#'   dyAxis("y", valueRange = c(0, 100)) %>%
+#'   dyEvent(c("1950-6-30", "1965-2-09"), c("Korea", "Vietnam"), labelLoc = "bottom") 
 #'  
 #' @export
 dyEvent <- function(dygraph, 
