66bf285e126e7ec93e95f009ae4fe99c8c834cad
Jeff Hanson Local Admin <jhanson@CBCS-ACADEM23L.uq.edu.au>
2016-10-14 18:28:56 +1000

    + added click callbacks

--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -24,5 +24,17 @@ shinyServer(function(input, output) {
   output$to <- renderText({
     if (!is.null(input$dygraph_date_window))
       strftime(input$dygraph_date_window[[2]], "%d %b %Y")
-  })  
+  })
+  
+  output$clicked <- renderText({
+	print(input$dygraph_click)
+	if (!is.null(input$dygraph_click))
+		strftime(input$dygraph_click$x, "%d %b %Y")
+  })
+
+  output$point <- renderText({
+	if (!is.null(input$dygraph_click))
+		paste0('X = ', strftime(input$dygraph_click$x_closest_point, "%d %b %Y"), '; Y = ', input$dygraph_click$y_closest_point)
+  })
+  
 })
