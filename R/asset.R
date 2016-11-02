6e29b4b50ec38591ce6139ceb33cc694164a500c
Petr Shevtsov <petr.shevtsov@gmail.com>
2016-11-03 02:34:21 +0300

    Adding dyAsset function
    dyAsset function is used to load external assets: plugins, plotters,
    data handlers, etc.

---
+++ R/asset.R
@@ -0,0 +1,108 @@
+#' Add an external JavaScript asset as a dygraph dependency
+#'
+#' @param dygraph Dygraph to add asset to
+#' @param type Type of asset (valid options are: 'plugins', 'plotter' or 'dataHandler')
+#' @param name Name of asset
+#' @param path Path to asset JavaScript file
+#' @param version Asset version (e.g. version of package which provides the asset)
+#'
+#' @return A dygraph with the specified asset added.
+#'
+#' @importFrom htmltools htmlDependency
+#'
+#' @export
+dyAsset <- function(dygraph, type, name, path, version = "1.0") {
+  # create an html dependency for the asset js file
+  path <- normalizePath(path)
+  assetDependency <- htmlDependency(depName(type, name),
+                                    version,
+                                    src = dirname(path),
+                                    script = basename(path),
+                                    all_files = FALSE)
+
+  # add the asset javascript to the dependencies
+  if (is.null(dygraph$dependencies)) {
+    dygraph$dependencies <- list()
+  }
+  dygraph$dependencies[[length(dygraph$dependencies) + 1]] <- assetDependency
+
+  # return dygraph
+  dygraph
+}
+
+#' Make up a htmlDependency name based on asset type and name
+#'
+#' @param type Type of asset (valid options are: 'plugins', 'plotter' or 'dataHandler')
+#' @param name Name of asset
+#'
+#' @return A htmlDependency name.
+depName <- function(type, name) {
+  switch(type,
+         plugins = paste0("Dygraph.Plugins.", name),
+         plotter = paste0("Dygraph.Plotters.", name),
+         dataHandler = paste0("Dygraph.DataHandlers.", name))
+}
+
+#' Include a dygraph plugin
+#'
+#' @param dygraph Dygraph to add plugin to
+#' @param name Name of plugin
+#' @param path Path to plugin JavaScript file
+#' @param options Named list of options to pass to plugin constructor
+#' @param version Plugin version (e.g. version of package which provides the plugin)
+#'
+#' @return A dygraph with the specified plugin enabled.
+#'
+#' @details
+#' You can use dygraphs plugins to customize the appearance of dygraphs as well
+#' as add new interactive behaviors. For additional information on creating
+#' dygraphs plugins see the
+#' \href{https://github.com/danvk/dygraphs/tree/master/src/plugins}{dygraphs plugins}
+#' documentation.
+#
+#' Once you've created a dygraphs plugins you can use the dyPlugin function to
+#' create an R wrapper for it. See the
+#' \href{https://github.com/rstudio/dygraphs/tree/master/inst/examples/plugins/plugins.R}{example plugins}
+#' for details on how to do this.
+#'
+#' @export
+dyPlugin <- function(dygraph, name, path, options = list(), version = "1.0") {
+  dygraph <- dyAsset(dygraph = dygraph,
+                     type = 'plugins',
+                     name = name,
+                     path = path,
+                     version = version)
+  # add the plugin and it's options (will be evaluated by renderValue)
+  if (is.null(dygraph$x$plugins)) {
+    dygraph$x$plugins <- list()
+  }
+  if (length(options) == 0) {
+    options <- JS("{}")
+  }
+  dygraph$x$plugins[[name]] <- options
+
+  # return dygraph
+  dygraph
+}
+
+#' Include a dygraph plotter
+#'
+#' @param dygraph Dygraph to add plotter to
+#' @param name Name of plotter
+#' @param path Path to plotter JavaScript file
+#' @param version Plotter version (e.g. version of package which provides the plotter)
+#'
+#' @return A dygraph with the specified plotter enabled.
+#'
+#' @export
+dyPlotter <- function(dygraph, name, path, version = "1.0") {
+  dygraph <- dyAsset(dygraph = dygraph,
+                     type = 'plotter',
+                     name = name,
+                     path = path,
+                     version = version)
+  dygraph$x$plotter <- name
+
+  # return dygraph
+  dygraph
+}
