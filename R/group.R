107b0f9457214e20f271b8030c93be5039f7d43f
klauk <laukhuf@gmail.com>
2017-07-13 17:02:54 -0400

    Added stacked plotters, updated series options handling, updated docs

--- R/group.R
+++ R/group.R
@@ -100,8 +100,7 @@ dyGroup <- function(dygraph,
   data <- attr(dygraph$x, "data")
   labels <- names(data)
 
-  if (length(plotter)>1)
-    stop('pass only a single plotter option')
+  if (length(plotter)>1) message('dyGroup: pass only a single plotter option')
   
   
   # auto-bind name if necessary
@@ -160,17 +159,27 @@ dyGroup <- function(dygraph,
     # take the passed options and extend to the length of the name vector; it's
     # up to the User to make sure the vectors are of the desired length
     suppressWarnings({
-      series$options$axis               <- rep(match.arg(axis, c("y", "y2")), 
-                                               length.out = l)[i]
-      series$options$stepPlot           <- rep(stepPlot, length.out = l)[i]
-      series$options$fillGraph          <- rep(fillGraph, length.out = l)[i]
-      series$options$drawPoints         <- rep(drawPoints, length.out = l)[i]
-      series$options$pointSize          <- rep(pointSize, length.out = l)[i]
-      series$options$strokeWidth        <- rep(strokeWidth, length.out = l)[i]
-      series$options$strokePattern      <- rep(resolveStrokePattern(strokePattern), 
-                                               length.out = l)[i]
-      series$options$strokeBorderWidth  <- rep(strokeBorderWidth, length.out = l)[i]
-      series$options$strokeBorderColor  <- rep(strokeBorderColor, length.out = l)[i]
+      # for the axis, however, we enforce the same axis across all series named
+      # in the group.  We can't stop the user from changing the axis of one or more
+      # series later, but at least we can control for some mistakes here
+      series$options$axis <- rep(match.arg(axis, c("y", "y2")), 
+                      			 			length.out = l)[1]
+      if(!is.null(stepPlot)) series$options$stepPlot <- rep(
+																	stepPlot, length.out = l)[i]
+      if(!is.null(fillGraph)) series$options$fillGraph <- rep(
+																	fillGraph, length.out = l)[i]
+      if(!is.null(drawPoints)) series$options$drawPoints <- rep(
+																	drawPoints, length.out = l)[i]
+      if(!is.null(pointSize)) series$options$pointSize <- rep(
+																	pointSize, length.out = l)[i]
+      if(!is.null(strokeWidth)) series$options$strokeWidth <- rep(
+																	strokeWidth, length.out = l)[i]
+      if(!is.null(strokePattern)) series$options$strokePattern <- rep(
+																	resolveStrokePattern(strokePattern), length.out = l)[i]
+      if(!is.null(strokeBorderWidth)) series$options$strokeBorderWidth <- rep(
+																	strokeBorderWidth, length.out = l)[i]
+      if(!is.null(strokeBorderColor)) series$options$strokeBorderColor <- rep(
+																	strokeBorderColor, length.out = l)[i]
      
       # one can use this to pass a group plotter or any combination of individual series plotters 
       series$options$plotter <- JS(plotter)
@@ -182,6 +191,7 @@ dyGroup <- function(dygraph,
     # passed gets re-passed together a second time, which would obviously override
     # the first set of options
     series$options$group <- paste0(name, collapse = "")
+  
     seriesData <- data[[series$name]]
     
     # default the label if we need to
@@ -190,6 +200,10 @@ dyGroup <- function(dygraph,
     
     # add label
     attrs$labels <- c(attrs$labels, series$label)
+    
+    # get whatever options might have previously existed for the series, then merge
+    base <- attrs$series[[series$label]]
+	 	series$options <- mergeLists(base, series$options)
     
     # set options
     attrs$series[[series$label]] <- series$options
@@ -225,257 +239,3 @@ dyGroup <- function(dygraph,
   dygraph
 }
 
-# Dygraph.prototype.computeYAxisRanges_ = function(extremes) {
-#   var isNullUndefinedOrNaN = function(num) {
-#     return isNaN(parseFloat(num));
-#   };
-#   var numAxes = this.attributes_.numAxes();
-#   var ypadCompat, span, series, ypad;
-#   
-#   var p_axis;
-# 
-#   // Compute extreme values, a span and tick marks for each axis.
-#   for (var i = 0; i < numAxes; i++) {
-#     var axis = this.axes_[i];
-#     var logscale = this.attributes_.getForAxis("logscale", i);
-#     var includeZero = this.attributes_.getForAxis("includeZero", i);
-#     var independentTicks = this.attributes_.getForAxis("independentTicks", i);
-#     series = this.attributes_.seriesForAxis(i);
-# 
-#     // Add some padding. This supports two Y padding operation modes:
-#     //
-#     // - backwards compatible (yRangePad not set):
-#     //   10% padding for automatic Y ranges, but not for user-supplied
-#     //   ranges, and move a close-to-zero edge to zero except if
-#     //   avoidMinZero is set, since drawing at the edge results in
-#     //   invisible lines. Unfortunately lines drawn at the edge of a
-#     //   user-supplied range will still be invisible. If logscale is
-#     //   set, add a variable amount of padding at the top but
-#     //   none at the bottom.
-#     //
-#     // - new-style (yRangePad set by the user):
-#     //   always add the specified Y padding.
-#     //
-#     ypadCompat = true;
-#     ypad = 0.1; // add 10%
-#     if (this.getNumericOption('yRangePad') !== null) {
-#       ypadCompat = false;
-#       // Convert pixel padding to ratio
-#       ypad = this.getNumericOption('yRangePad') / this.plotter_.area.h;
-#     }
-# 
-#     if (series.length === 0) {
-#       // If no series are defined or visible then use a reasonable default
-#       axis.extremeRange = [0, 1];
-#     } else {
-#       // Calculate the extremes of extremes.
-#       var minY = Infinity;  // extremes[series[0]][0];
-#       var maxY = -Infinity;  // extremes[series[0]][1];
-#       var extremeMinY, extremeMaxY;
-# 
-#       for (var j = 0; j < series.length; j++) {
-#         // this skips invisible series
-#         if (!extremes.hasOwnProperty(series[j])) continue;
-# 
-#         // Only use valid extremes to stop null data series' from corrupting the scale.
-#         extremeMinY = extremes[series[j]][0];
-#         if (extremeMinY !== null) {
-#           minY = Math.min(extremeMinY, minY);
-#         }
-#         extremeMaxY = extremes[series[j]][1];
-#         if (extremeMaxY !== null) {
-#           maxY = Math.max(extremeMaxY, maxY);
-#         }
-#       }
-# 
-#       // Include zero if requested by the user.
-#       if (includeZero && !logscale) {
-#         if (minY > 0) minY = 0;
-#         if (maxY < 0) maxY = 0;
-#       }
-# 
-#       // Ensure we have a valid scale, otherwise default to [0, 1] for safety.
-#       if (minY == Infinity) minY = 0;
-#       if (maxY == -Infinity) maxY = 1;
-# 
-#       span = maxY - minY;
-#       // special case: if we have no sense of scale, center on the sole value.
-#       if (span === 0) {
-#         if (maxY !== 0) {
-#           span = Math.abs(maxY);
-#         } else {
-#           // ... and if the sole value is zero, use range 0-1.
-#           maxY = 1;
-#           span = 1;
-#         }
-#       }
-# 
-#       var maxAxisY, minAxisY;
-#       if (logscale) {
-#         if (ypadCompat) {
-#           maxAxisY = maxY + ypad * span;
-#           minAxisY = minY;
-#         } else {
-#           var logpad = Math.exp(Math.log(span) * ypad);
-#           maxAxisY = maxY * logpad;
-#           minAxisY = minY / logpad;
-#         }
-#       } else {
-#         maxAxisY = maxY + ypad * span;
-#         minAxisY = minY - ypad * span;
-# 
-#         // Backwards-compatible behavior: Move the span to start or end at zero if it's
-#         // close to zero, but not if avoidMinZero is set.
-#         if (ypadCompat && !this.getBooleanOption("avoidMinZero")) {
-#           if (minAxisY < 0 && minY >= 0) minAxisY = 0;
-#           if (maxAxisY > 0 && maxY <= 0) maxAxisY = 0;
-#         }
-#       }
-#       axis.extremeRange = [minAxisY, maxAxisY];
-#     }
-#     if (axis.valueWindow) {
-#       // This is only set if the user has zoomed on the y-axis. It is never set
-#       // by a user. It takes precedence over axis.valueRange because, if you set
-#       // valueRange, you'd still expect to be able to pan.
-#       axis.computedValueRange = [axis.valueWindow[0], axis.valueWindow[1]];
-#     } else if (axis.valueRange) {
-#       // This is a user-set value range for this axis.
-#       var y0 = isNullUndefinedOrNaN(axis.valueRange[0]) ? axis.extremeRange[0] : axis.valueRange[0];
-#       var y1 = isNullUndefinedOrNaN(axis.valueRange[1]) ? axis.extremeRange[1] : axis.valueRange[1];
-#       if (!ypadCompat) {
-#         if (axis.logscale) {
-#           var logpad = Math.exp(Math.log(span) * ypad);
-#           y0 *= logpad;
-#           y1 /= logpad;
-#         } else {
-#           span = y1 - y0;
-#           y0 -= span * ypad;
-#           y1 += span * ypad;
-#         }
-#       }
-#       axis.computedValueRange = [y0, y1];
-#     } else {
-#       axis.computedValueRange = axis.extremeRange;
-#     }
-#     
-#     
-#     if (independentTicks) {
-#       axis.independentTicks = independentTicks;
-#       var opts = this.optionsViewForAxis_('y' + (i ? '2' : ''));
-#       var ticker = opts('ticker');
-#       axis.ticks = ticker(axis.computedValueRange[0],
-#               axis.computedValueRange[1],
-#               this.plotter_.area.h,
-#               opts,
-#               this);
-#       // Define the first independent axis as primary axis.
-#       if (!p_axis) p_axis = axis;
-#     }
-#   }
-#   if (p_axis === undefined) {
-#     throw ("Configuration Error: At least one axis has to have the \"independentTicks\" option activated.");
-#   }
-#   // Add ticks. By default, all axes inherit the tick positions of the
-#   // primary axis. However, if an axis is specifically marked as having
-#   // independent ticks, then that is permissible as well.
-#   for (var i = 0; i < numAxes; i++) {
-#     var axis = this.axes_[i];
-#     
-#     if (!axis.independentTicks) {
-#       var opts = this.optionsViewForAxis_('y' + (i ? '2' : ''));
-#       var ticker = opts('ticker');
-#       var p_ticks = p_axis.ticks;
-#       var p_scale = p_axis.computedValueRange[1] - p_axis.computedValueRange[0];
-#       var scale = axis.computedValueRange[1] - axis.computedValueRange[0];
-#       var tick_values = [];
-#       for (var k = 0; k < p_ticks.length; k++) {
-#         var y_frac = (p_ticks[k].v - p_axis.computedValueRange[0]) / p_scale;
-#         var y_val = axis.computedValueRange[0] + y_frac * scale;
-#         tick_values.push(y_val);
-#       }
-# 
-#       axis.ticks = ticker(axis.computedValueRange[0],
-#                           axis.computedValueRange[1],
-#                           this.plotter_.area.h,
-#                           opts,
-#                           this,
-#                           tick_values);
-#     }
-#   }
-# };
-
-# Dygraph.stackPoints_ = function(
-#     points, cumulativeYval, seriesExtremes, fillMethod) {
-#   var lastXval = null;
-#   var prevPoint = null;
-#   var nextPoint = null;
-#   var nextPointIdx = -1;
-# 
-#   // Find the next stackable point starting from the given index.
-#   var updateNextPoint = function(idx) {
-#     // If we've previously found a non-NaN point and haven't gone past it yet,
-#     // just use that.
-#     if (nextPointIdx >= idx) return;
-# 
-#     // We haven't found a non-NaN point yet or have moved past it,
-#     // look towards the right to find a non-NaN point.
-#     for (var j = idx; j < points.length; ++j) {
-#       // Clear out a previously-found point (if any) since it's no longer
-#       // valid, we shouldn't use it for interpolation anymore.
-#       nextPoint = null;
-#       if (!isNaN(points[j].yval) && points[j].yval !== null) {
-#         nextPointIdx = j;
-#         nextPoint = points[j];
-#         break;
-#       }
-#     }
-#   };
-# 
-#   for (var i = 0; i < points.length; ++i) {
-#     var point = points[i];
-#     var xval = point.xval;
-#     if (cumulativeYval[xval] === undefined) {
-#       cumulativeYval[xval] = 0;
-#     }
-# 
-#     var actualYval = point.yval;
-#     if (isNaN(actualYval) || actualYval === null) {
-#       if(fillMethod == 'none') {
-#         actualYval = 0;
-#       } else {
-#         // Interpolate/extend for stacking purposes if possible.
-#         updateNextPoint(i);
-#         if (prevPoint && nextPoint && fillMethod != 'none') {
-#           // Use linear interpolation between prevPoint and nextPoint.
-#           actualYval = prevPoint.yval + (nextPoint.yval - prevPoint.yval) *
-#               ((xval - prevPoint.xval) / (nextPoint.xval - prevPoint.xval));
-#         } else if (prevPoint && fillMethod == 'all') {
-#           actualYval = prevPoint.yval;
-#         } else if (nextPoint && fillMethod == 'all') {
-#           actualYval = nextPoint.yval;
-#         } else {
-#           actualYval = 0;
-#         }
-#       }
-#     } else {
-#       prevPoint = point;
-#     }
-# 
-#     var stackedYval = cumulativeYval[xval];
-#     if (lastXval != xval) {
-#       // If an x-value is repeated, we ignore the duplicates.
-#       stackedYval += actualYval;
-#       cumulativeYval[xval] = stackedYval;
-#     }
-#     lastXval = xval;
-# 
-#     point.yval_stacked = stackedYval;
-# 
-#     if (stackedYval > seriesExtremes[1]) {
-#       seriesExtremes[1] = stackedYval;
-#     }
-#     if (stackedYval < seriesExtremes[0]) {
-#       seriesExtremes[0] = stackedYval;
-#     }
-#   }
-# };
--- R/plotters.R
+++ R/plotters.R
@@ -13,6 +13,8 @@
 #' \describe{
 #'    \item{dyBarChart()}{Draws a bar plot rather than a line plot. If the provided dygraph
 #' features more than one series, dyBarChart will call dyMultiColumn instead.}
+#' 
+#'    \item{dyStackedBarChart()}{Draws a bar chart stacking all the underlying series.}
 #'
 #'    \item{dyMultiColumn()}{Draws multiple column bar chart.} 
 # 
@@ -33,6 +35,10 @@
 #' 
 #'    \item{dyCandleStickGroup()}{Employed on the provided series, 
 #'    but still plotting the others.}
+#'    
+#'    \item{dyStackerBarGroup()}{Return the data group as stacked bars}
+#' 
+#'    \item{dyStackerRibbonGroup()}{Return the data group as stacked ribbons}
 #' 
 #' }
 #' 
@@ -49,40 +55,39 @@
 #' @name Plotters
 #' @examples 
 #' ## The following two examples will results in the same dygraph:
+#' 
+#' dygraph(mdeaths) %>% 
+#'   dyBarChart()
 #'  
 #' lungDeaths <- cbind(mdeaths, fdeaths)
 #' dygraph(lungDeaths) %>%
-#'   dyRangeSelector() %>%
 #'   dyMultiColumn()
 #'   
 #' lungDeaths <- cbind(mdeaths, fdeaths)
 #' dygraph(lungDeaths) %>%
-#'   dyRangeSelector() %>%
-#'   dyBarChart()
+#'   dyStackedBarChart()
+#'
 #'   
 #' ## Per-series plotters:
 #' 
 #' lungDeaths <- cbind(mdeaths, fdeaths)
 #' dygraph(lungDeaths) %>%
-#'   dyRangeSelector() %>%
 #'   dyBarSeries('fdeaths')
 #' 
 #' lungDeaths <- cbind(mdeaths, fdeaths)
 #' dygraph(lungDeaths) %>%
-#'   dyRangeSelector() %>%
 #'   dyStemSeries('fdeaths')
 #'   
 #' lungDeaths <- cbind(mdeaths, fdeaths)
 #' dygraph(lungDeaths) %>%
-#'   dyRangeSelector() %>%
 #'   dyShadow('fdeaths')
 #' 
 #' lungDeaths <- cbind(mdeaths, fdeaths)
 #' dygraph(lungDeaths) %>%
-#'   dyRangeSelector() %>%
 #'   dyFilledLine('fdeaths')
+#'
 #'   
-#' ## A bunch of different plotters together
+#' ## A bunch of different plotters together:
 #' 
 #' lungDeaths <- cbind(fdeaths, mdeaths, ldeaths, foo = fdeaths/2, bar = fdeaths/3)
 #' dygraph(lungDeaths) %>%
@@ -91,12 +96,14 @@
 #'   dyStemSeries('mdeaths') %>% 
 #'   dyShadow('foo') %>% 
 #'   dyFilledLine('fdeaths')
+#'
 #'   
 #' ## Group-based plotters:
 #'   
-#' lungDeaths <- cbind(mdeaths, fdeaths, ldeaths)
-#' dygraph(lungDeaths) %>% 
-#'   dyMultiColumnGroup(c('fdeaths', 'mdeaths'))
+# lungDeaths <- cbind(mdeaths, fdeaths, ldeaths)
+# dygraph(lungDeaths) %>%
+#   dyMultiColumnGroup(c('fdeaths', 'mdeaths'))
+#'
 #'
 #' ## Candlestick plotters:
 #'   
@@ -114,6 +121,23 @@
 #'   dyOptions(stackedGraph = TRUE) %>% 
 #'   dyCandlestickGroup(c('Open', 'High', 'Low', 'Close')) %>% 
 #'   dyCandlestickGroup(c('O', 'H', 'L', 'C'))
+#'
+#' 
+#' ## Stacked Bar and Ribbon Graphs:  
+#' 
+#' dygraph(lungDeaths) %>% 
+#'   dySeries('mdeaths', axis = 'y2') %>%
+#'   dyAxis('y', valueRange = c(-100, 1000)) %>% 
+#'   dyStackedBarGroup(c('ldeaths', 'fdeaths'))
+#'   
+#' lungDeaths <- cbind(ldeaths, fdeaths, mdeaths, 
+#'                     additive = rep.int(200, length(ldeaths)),
+#'                     line = rep.int(3000, length(ldeaths)))
+#' dygraph(lungDeaths) %>% 
+#'   dySeries('line', strokePattern = 'dashed') %>% 
+#'   dySeries('ldeaths', stepPlot = TRUE) %>% 
+#'   dyStackedBarGroup(c('additive', 'mdeaths')) %>% 
+#'   dyStackedRibbonGroup(c('fdeaths', 'ldeaths'))
 NULL
 
 #' @rdname Plotters
@@ -136,66 +160,81 @@ dyBarChart <- function(dygraph) {
 
 #' @rdname Plotters
 #' @export
+dyStackedBarChart <- function(dygraph) {
+  dygraph <- dyPlotter(dygraph = dygraph,
+              name = "StackedBarChart",
+              path = system.file("plotters/stackedbarchart.js", package = "dygraphs"))
+  
+  names<-dygraph$x$attrs$labels[-1]
+  
+  dygraph <- computeYaxisRange(dygraph, names)
+  dygraph 
+  
+}
+
+
+#' @rdname Plotters
+#' @export
 dyMultiColumn <- function(dygraph) {
   dyPlotter(dygraph = dygraph,
             name = "MultiColumn",
             path = system.file("plotters/multicolumn.js", package = "dygraphs"))
 }
 
+
 #' @rdname Plotters
 #' @export
 dyBarSeries <- function(dygraph, name, ...) {
   file <- system.file("plotters/barseries.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-
-  dySeries(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_,
-           ...)
+  
+  dots <- list(...)
+  do.call('dySeries', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
+  
 }
+
 
 #' @rdname Plotters
 #' @export
 dyStemSeries <- function(dygraph, name, ...) {
   file <- system.file("plotters/stemplot.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-  dySeries(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_, 
-           ...)
+  
+  dots <- list(...)
+  do.call('dySeries', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
 }
+
 
 #' @rdname Plotters
 #' @export
 dyShadow <- function(dygraph, name, ...) {
   file <- system.file("plotters/fillplotter.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-  dySeries(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_, 
-           ...)
+  
+  dots <- list(...)
+  do.call('dySeries', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
 }
+
 
 #' @rdname Plotters
 #' @export
 dyFilledLine <- function(dygraph, name, ...) {
   file <- system.file("plotters/filledline.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-  dySeries(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_, 
-           ...)
+  
+  dots <- list(...)
+  do.call('dySeries', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
 }
+
 
 #' @rdname Plotters
 #' @export
 dyErrorFill <- function(dygraph, name, ...) {
   file <- system.file("plotters/errorplotter.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-  dySeries(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_, 
-           ...)
+  
+  dots <- list(...)
+  do.call('dySeries', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
 }
 
 
@@ -204,33 +243,159 @@ dyErrorFill <- function(dygraph, name, ...) {
 dyMultiColumnGroup <- function(dygraph, name, ...) {
   file <- system.file("plotters/multicolumngroup.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-  dyGroup(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_, 
-           ...)
   
+  dots <- list(...)
+  do.call('dyGroup', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
 }
+
 
 #' @rdname Plotters
 #' @export
 dyCandlestickGroup <- function(dygraph, name, ...) {
   file <- system.file("plotters/candlestickgroup.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-  dyGroup(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_, 
-           ...)
+  
+  dots <- list(...)
+  do.call('dyGroup', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
 }
 
 
 #' @rdname Plotters
 #' @export
 dyStackedBarGroup <- function(dygraph, name, ...) {
+  dots <- list(...)
+  
+  if(length(name) < 2) {
+    dygraph <- do.call('dyBarSeries', c(list(dygraph = dygraph, name = unlist(name)), dots))
+    return(dygraph)
+  }
+  
   file <- system.file("plotters/stackedbargroup.js", package = "dygraphs")
   plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
-  dyGroup(dygraph = dygraph, 
-           name = name, 
-           plotter = plotter_, 
-           ...)
   
+  dygraph <- do.call('dyGroup', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
+
+  dygraph <- computeYaxisRange(dygraph, name)
+  dygraph 
+}
+
+#' @rdname Plotters
+#' @export
+dyStackedLineGroup <- function(dygraph, name, ...) {
+  dots <- list(...)
+  
+  if(length(name) < 2) {
+    # just rely on the base package
+    return(dygraph)
+  }
+  
+  file <- system.file("plotters/stackedlinegroup.js", package = "dygraphs")
+  plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
+  
+  dygraph <- do.call('dyGroup', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
+
+  dygraph <- computeYaxisRange(dygraph, name)
+  dygraph 
+}
+ 
+ 
+#' @rdname Plotters
+#' @export
+dyStackedRibbonGroup <- function(dygraph, name, ...) {
+  dots <- list(...)
+  
+  if(length(name) < 2) {
+    dygraph <- do.call('dyFilledLine', c(list(dygraph = dygraph, name = name), dots))
+    return(dygraph)
+  }
+  
+  file <- system.file("plotters/stackedribbongroup.js", package = "dygraphs")
+  plotter_ <- paste0(readLines(file, skipNul = T), collapse = "\n")
+  
+  dygraph <- do.call('dyGroup', c(list(dygraph = dygraph, name = name, plotter = plotter_), dots))
+
+  dygraph <- computeYaxisRange(dygraph, name)
+  dygraph 
+}
+
+computeYaxisRange <- function(dygraph, name){ 
+  # most of what happens from here on out is a simplified version of the 
+  # stackPoints and computeYaxis functions in the underlying dygraph package.
+  # Since we can't modify the Yaxis range from within the specialized plotter,
+  # we need to calculate an appropriate valueRange for the group's axis here, then
+  # reconcile that against user-provided ranges and then pass into the widget... this
+  # way we ensure that the newly stacked data (but initially unstacked to the widget) 
+  # doesn't get cutoff by an axis range - computed by the widget - that won't consider
+  # the points stacked
+  attrs <- dygraph$x$attrs
+
+  data <- attr(dygraph$x, "data")
+  cols <- which(names(data) %in% name)
+  
+  #get all the series, minus the x axis, that are not part of the group
+  data_ <- data[-c(1, cols)]
+  name_ <- names(data)[which(!names(data) %in% name)][-1]
+  
+  #grab the axis for the group, we'll calculate the new range only on this axis  
+  series <- attrs$series[[name[1]]]
+  axisNm <- series$axis
+  if (!is.null(axisNm)) {
+    axis <- attrs$axes[[axisNm]]
+    valueRange <- axis$valueRange
+  } else {
+    axisNm<-"y"
+    axis <- attrs$axes[[axisNm]] <- NULL
+    valueRange <- NULL
+  }
+  
+  if(is.null(valueRange)) {
+    valueRange <- c(0, 0)
+  }
+ 
+  # get the group data fields
+  data <- data[cols]
+ 
+  for(i in 1:length(data)) {
+      # get the data points
+      points <- data[i][[1]]
+      # fill NAs... we're not saving this data back into the graph, so this is OK
+      is.na(points) <- 0
+      
+      # add to cumulativeList
+      if(!exists('cumulativeYval')) cumulativeYval <- points
+      else cumulativeYval <- cumulativeYval + points
+      # calculate extremes
+      extremes <- range(cumulativeYval)
+  }
+  
+  for(i in 1:length(data_)) {
+    if (length(data_)==0) break
+    
+    # ranges are calcuated separately, so skip those from other axes
+    series_ <- attrs$series[[name_[i]]]
+    if (!is.null(series_$axis) && series_$axis != axisNm) next
+    
+    points <- data_[i][[1]]
+  
+    # fill NAs
+    is.na(points) <- 0
+    
+    # getExtremes
+    extremes_ <- range(points)
+    extremes[1] <- min(extremes[1], extremes_[1])  
+    extremes[2] <- max(extremes[2], extremes_[2])  
+  }
+  
+  valueRange[1] <- min(extremes[1], valueRange[1])
+  valueRange[2] <- max(extremes[2], valueRange[2])
+    
+  # add a little padding since we're hard-setting the range
+  valueRange[2] <- valueRange[2] + 0.05 * abs(valueRange[2] - valueRange[1])
+  
+  axis$options$valueRange <- valueRange
+  attrs$axes[[axisNm]] <- axis$options  
+  
+  # return modified dygraph
+  dygraph$x$attrs <- mergeLists(dygraph$x$attrs, attrs)
+  return (dygraph)
 }
